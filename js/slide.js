af110ab0fb5b95782235e1b5654b8b002d97eb58
Carine Batista <carinedcb@hotmail.com>
2021-05-16 20:12:23 -0300

    Add slides config

--- js/slide.js
+++ js/slide.js
@@ -55,9 +55,41 @@ export default class Slide{
 		}
 
 
+		// Slide Position
+		slidePosition(slide){
+			const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2
+			return -(slide.offsetLeft - margin)
+		}
+
+		// Slides Config
+		slidesConfig(){
+			const slideArray = [...this.slide.children].map((element) => {
+				const position = this.slidePosition(element)
+				return {position, element}
+			})
+		}
+
+		slidesIndexNav(index){
+			const last = this.slideArray.length - 1;
+			this.index = {
+				prev: index ? index - 1 : undefined, 
+				active: index, 
+				next: index === last ? undefined : index + 1,
+			}
+		}
+
+		changeSlide(index) {
+			const activeSlide = this.slideArray[index]
+			this.moveSlide(activeSlide.position)
+			this.slidesIndexNav(index)
+			this.dist.finalPosition = activeSlide.position
+			console.log(this.index)
+		}
+
 		init(){
 			this.bindEvents()
 			this.addSlideEvents()
+			this.slidesConfig()
 			return this
 		}
 
