8fe64d887c4cbebbd6473f1fd3b495d2541f4ec0
Carine Batista <carinedcb@hotmail.com>
2021-05-17 15:59:45 -0300

    Add Custom Controls

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-import {SlideNav} from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav('.wrapper', '.slide')
 slide.init()
 slide.addArrow('.prev', '.next')
-
-slide.addControl()
+slide.addControl('.custom-control')
--- js/slide.js
+++ js/slide.js
@@ -152,7 +152,7 @@ export class Slide{
 }
 
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
 
 	constructor(slide, wrapper){
 		super(slide, wrapper)
